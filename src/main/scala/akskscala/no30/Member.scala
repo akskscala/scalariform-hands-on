fbd7c8812dbf8d15ba970eb3f4657268c5d53737
Kazuhiro Sera <seratch@gmail.com>
2012-02-02 20:57:47 +0900

    ignore .cache

--- src/main/scala/akskscala/no30/Member.scala
+++ src/main/scala/akskscala/no30/Member.scala
@@ -1,11 +1,14 @@
 package akskscala.no30
 
-/**あかさかすから
-*乱れたコメント・・*/
+/**
+ * あかさかすから
+ * 乱れたコメント・・
+ */
 class Member(name: String, age: Int) {
 
   def selfIntroduction(): String = {
     "Nice to meet you! My name is " + name + ". I am " + age + " years old."
+
   }
 
 }
